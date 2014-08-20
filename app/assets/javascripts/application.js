7fe3dd6d8873c0a0958256c8035864a8df944fcd
ensonmj <ensonmj@gmail.com>
2014-08-21 00:12:00 +0800

    get codemirror worked

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,6 +19,7 @@
 //= require codemirror
 //= require codemirror/modes/xml
 //= require codemirror/modes/markdown
+//= require codemirror/addons/mode/overlay
 //= require codemirror/modes/gfm
 //= require codemirror/modes/javascript
 //= require js-deflate/rawdeflate
