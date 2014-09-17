9d4783da15e551f1ac1fa9a7a60145a45e72b456
ensonmj <ensonmj@gmail.com>
2014-09-17 15:54:49 +0800

    fix google analytics

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,9 +1,3 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // compiled file.
 //
@@ -26,3 +20,29 @@
 //= require js-deflate/rawdeflate
 //= require js-deflate/rawinflate
 //= require bootstrap-sprockets
+
+<!-- marked -->
+$(document).ready(function(){
+  var blocks = document.querySelectorAll('.se_text');
+  Array.prototype.forEach.call(blocks, function(b){
+    b.innerHTML = marked(b.innerHTML);
+  });
+});
+
+<!-- highlight -->
+$(document).ready(function(){
+  var blocks = document.querySelectorAll("pre code");
+  Array.prototype.forEach.call(blocks, function(b){
+    hljs.highlightBlock(b);
+  });
+});
+
+<!--google universal analytics-->
+(function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,"script","//www.google-analytics.com/analytics.js","ga");
+ga("create", "<%= Settings.google_analytics_key %>", "auto");
+$(document).ready(function(){
+  ga("send", "pageview", window.location.pathname);
+});
