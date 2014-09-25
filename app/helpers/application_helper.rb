fdf65aa72068876af350db557780b0c1dbbb7062
ensonmj <ensonmj@gmail.com>
2014-09-25 16:06:23 +0800

    prettify flash

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,9 +4,18 @@ module ApplicationHelper
   end
 
   def bootstrap_class_for flash_type
-    {success: "alert-success",
-     error: "alert-danger",
-     alert:  "alert-warning",
-     notice: "alert-info"}[flash_type.to_sym] || flash_type.to_s
+    { success: "alert-success",
+      error: "alert-danger",
+      alert:  "alert-warning",
+      notice: "alert-info"
+    }[flash_type.to_sym] || flash_type.to_s
+  end
+
+  def bootstrap_icon_for flash_type
+    { success: "ok-circle",
+      error: "remove-circle",
+      alert: "warning-sing",
+      notice: "exclamation-sign"
+    }[flash_type.to_sym] || "question-sign"
   end
 end
