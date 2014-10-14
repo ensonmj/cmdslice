131b85517b06833a5db620e1638dd89384c9da75
ensonmj <ensonmj@gmail.com>
2014-10-14 17:31:28 +0800

    validate password length for registration

--- app/models/identity.rb
+++ app/models/identity.rb
@@ -3,4 +3,5 @@ class Identity < OmniAuth::Identity::Models::ActiveRecord
   validates_uniqueness_of :email
   validates_format_of :email,
     :with => /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i
+  validates_length_of :password, in: 6..20
 end
