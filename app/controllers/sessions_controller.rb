c031ec8b706029535da2043baf4f4c5dfe09e9d9
ensonmj <ensonmj@gmail.com>
2014-10-15 12:03:30 +0800

    add some comment for session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,7 @@ class SessionsController < ApplicationController
   def new
   end
 
+  # after omniauth process /auth/:provider/callback(.format) success
   def create
     user = User.from_omniauth(request.env['omniauth.auth'])
     session[:user_id] = user.id
@@ -13,6 +14,7 @@ class SessionsController < ApplicationController
     redirect_to root_path, notice: "Signed out!"
   end
 
+  # omniauth process /auth/:provider/callback(.format) failure
   def failure
     redirect_to login_path,
       alert: "Authentication failed, please try again."
