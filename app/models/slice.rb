37becccce44971edc4ecf150efa7a91123381f89
ensonmj <ensonmj@gmail.com>
2014-10-30 16:34:00 +0800

    add some validation to slice

--- app/models/slice.rb
+++ app/models/slice.rb
@@ -1,6 +1,7 @@
 class Slice < ActiveRecord::Base
   has_many :comments, :dependent => :destroy
-  validates :title, presence: true, length: {minimum: 5}
+  validates :title, presence: true, length: {minimum: 2}
+  validates_presence_of :text
   belongs_to :user
 
   def user?(user)
