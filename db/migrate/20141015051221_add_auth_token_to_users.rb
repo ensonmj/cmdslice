287fb6bd573a7c517d3dd433f5a96b6079916690
ensonmj <ensonmj@gmail.com>
2014-10-31 15:01:13 +0800

    generate auth_token for exsit user after migration

--- db/migrate/20141015051221_add_auth_token_to_users.rb
+++ db/migrate/20141015051221_add_auth_token_to_users.rb
@@ -1,5 +1,9 @@
 class AddAuthTokenToUsers < ActiveRecord::Migration
   def change
     add_column :users, :auth_token, :string
+    User.all.each do |u|
+      u.generate_token(:auth_token)
+      u.save!
+    end
   end
 end
