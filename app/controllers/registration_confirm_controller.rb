fe14c58387ece4b1304224d079c664ee081413d7
ensonmj <ensonmj@gmail.com>
2014-11-03 18:36:51 +0800

    check whether confirmed

--- app/controllers/registration_confirm_controller.rb
+++ app/controllers/registration_confirm_controller.rb
@@ -3,8 +3,10 @@ class RegistrationConfirmController < ApplicationController
 
   def update
     @identity = Identity.includes(:user).find_by!(confirm_token: params[:id])
-    if @identity.confirm_sent_at < 2.hours.ago
-      redirect_to ruser_path(@identity.user), alert: "Confirm code has expired.
+    if @identity.confirmed?
+      redirect_to user_path(@identity.user), notice: "You have already confirmed."
+    elsif @identity.confirm_sent_at < 2.hours.ago
+      redirect_to user_path(@identity.user), alert: "Confirm code has expired.
        please request another code."
     elsif @identity.registration_confirm
       redirect_to user_path(@identity.user), notice: "You have confirmed, please enjoy yourself."
